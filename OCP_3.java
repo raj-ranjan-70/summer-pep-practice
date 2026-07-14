1c6164751a19e99ad8655ed42694c255ec8cfb3c
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-07-14 10:30:32 +0530

    Added OCP from SOLID principles Again

---
+++ OCP_3.java
@@ -0,0 +1,27 @@
+Interface ImageExporter {
+    void exportImage();
+}
+
+class PNGExporter implements ImageExporter {
+
+    @Override
+    public void exportImage() {
+        // Code to export image in PNG format...
+    }
+}
+
+class JPGExporter implements ImageExporter {
+
+    @Override
+    public void exportImage() {
+        // Code to export image in JPG format...
+    }
+}
+
+class JPEGExporter implements ImageExporter {
+
+    @Override
+    public void exportImage() {
+        // Code to export image in JPEG format...
+    }
+}
