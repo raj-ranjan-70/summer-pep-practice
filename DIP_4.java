f4d4eba06bdc2158f1c38e091e81e0202ce13b17
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-07-14 10:52:22 +0530

    Added DIP from SOLID principles

---
+++ DIP_4.java
@@ -0,0 +1,39 @@
+Interface payment {
+    vioid pay();
+}
+
+Interface Notification {
+    void send();
+}
+
+class StripePayment implements payment {
+    
+    @Override
+    public void pay() {
+        // Code to process payment using Stripe...
+    }
+}
+
+class EmailNotification implements Notification {
+
+    @Override
+    public void send() {
+        // Code to send email notification...
+    }
+}
+
+class orderservice {
+    
+    private payment paymentMethod;
+    private Notification notificationMethod;
+
+    public orderservice(payment paymentMethod, Notification notificationMethod) {
+        this.paymentMethod = paymentMethod;
+        this.notificationMethod = notificationMethod;
+    }
+
+    public void processOrder() {
+        paymentMethod.pay();
+        notificationMethod.send();
+    }
+}
