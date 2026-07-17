195c33e9aa7eb41ad50e490cddb4f33da553a156
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-07-17 13:50:21 +0530

    Added Java Classes and Object files

---
+++ java-practice/src/main/java/basicCodes/Car.java
@@ -0,0 +1,65 @@
+package basicCodes;
+
+public class Car {
+    private int year;
+    private String modelName;
+    private String color;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    Car(int year, String modelName, String color) {
+        this.year = year;
+        this.modelName = modelName;
+        this.color = color;
+    }
+
+    void driving() {
+        System.out.println("The Car with model name " + this.getModelName() + " is running");
+    }
+
+    void accelerate(int rate) {
+        System.out.println("The car is accelerating at the rate of " + rate + " meter per second square");
+    }
+
+    void decelerate(int rate) {
+        System.out.println("The car is deaccelerating at the rate of " + rate + " meter per second square");
+    }
+
+    void carDetails() {
+        System.out.println("\n.....The Car Details.....\n");
+        System.out.println("The car Model name is " + this.getModelName() + ".");
+        System.out.println("The car Year is " + this.getYear() + ".");
+        System.out.println("The car Color is " + this.getColor() + ".");
+    }
+
+
+    public static void main() {
+        Car car1 = new Car(2004, "Maruti Alto", "Red");
+        car1.driving();
+        car1.accelerate(60);
+        car1.decelerate(40);
+        car1.carDetails();
+    }
+}
---
+++ java-practice/src/main/java/basicCodes/Employee.java
@@ -0,0 +1,83 @@
+package basicCodes;
+
+public class Employee {
+
+    private String id;
+    private String name;
+    private String role;
+    private String dept;
+
+    Employee() {
+        this.id = "emp-def";
+        this.name = "John Doe";
+        this.role = "System Engineer";
+        this.dept = "Computer Science";
+    }
+
+    Employee(String id, String name, String role, String dept) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.dept = dept;
+    }
+
+    Employee(Employee employee) {
+        this(employee.id, employee.name, employee.role, employee.dept);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public void displayInfo() {
+        System.out.println("\n-----Employee Information-----\n");
+
+        System.out.println("Employee ID: " + this.id + ".");
+        System.out.println("Employee Name: " + this.name + ".");
+        System.out.println("Employee Role: " + this.role + ".");
+        System.out.println("Employee Dept: " + this.dept + ".");
+    }
+
+    static void main() {
+        Employee employee1 = new Employee();
+        employee1.displayInfo();
+        employee1.setId("emp-001");
+        employee1.setName("Raushan Kumar");
+        employee1.displayInfo();
+
+
+        Employee employee2 = new Employee("emp-002", "Raj Ranjan", "SDE1", "Computer Science");
+        employee2.displayInfo();
+        employee2.setRole("SDE2");
+        employee2.setDept("Artificial Intelligence");
+        employee2.displayInfo();
+    }
+}
